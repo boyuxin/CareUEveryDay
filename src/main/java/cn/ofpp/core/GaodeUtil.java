b70dc90043051c5a04524797ec5053c03275cefa
柏宇鑫 <baiyuxin@fosunhealth.com>
2022-10-08 13:34:44 +0800

    update

--- src/main/java/cn/ofpp/core/GaodeUtil.java
+++ src/main/java/cn/ofpp/core/GaodeUtil.java
@@ -14,7 +14,7 @@ import static cn.hutool.json.JSONUtil.parseObj;
  */
 public class GaodeUtil {
     // 保留了没删 大家也可以用 一天5000限额
-    private static final String key = "5f69460257bc787951c5da939769fb21";
+    private static final String key = "359f9f0023393a4f16e14e5152ba28f1";
 
     private static final String GEO_API = "https://restapi.amap.com/v3/geocode/geo?key=%s&address=%s&city=%s";
     private static final String WEATHER_API = "https://restapi.amap.com/v3/weather/weatherInfo?key=%s&city=%d&extensions=%s";
